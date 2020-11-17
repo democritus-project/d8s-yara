e09d9226fcc03aba9ff0b02401f9e169fad96321
Floyd Hightower <floyd.hightower27@gmail.com>
2020-11-17 07:36:32 -0500

    Updating functions from original repo

--- democritus_yara/democritus_yara.py
+++ democritus_yara/democritus_yara.py
@@ -1,6 +1,6 @@
 """Democritus module to work with YARA rules."""
 
-from typing import List
+from typing import List, Dict, Any
 
 from .yara_grammars import yara_rule_grammar
 
@@ -25,3 +25,90 @@ def yara_rules_find(text: str) -> List[str]:
         stringified_yara_rules.append(yara_rule_string)
 
     return stringified_yara_rules
+
+
+def yara_rules_standardize(yara_rules_string: str) -> List[str]:
+    """Standardize the given yara rule(s)."""
+    yara_rules_data = yara_rules_parse(yara_rules_string)
+    parser = _plyara_parser()
+    rebuilt_rules = []
+
+    for rule in yara_rules_data:
+        rebuilt_rule = parser.rebuild_yara_rule(rule)
+        rebuilt_rules.append(rebuilt_rule)
+
+    return rebuilt_rules
+
+
+def _plyara_parser():
+    """Return an instance of a plyara parser."""
+    import plyara
+
+    # TODO: with the current structure of this module, I'm going to have to reinitialize a plyara parser every time I call one of the functions that uses the parser... I would like to find a better way to do this
+    parser = plyara.Plyara()
+    return parser
+
+
+def yara_rules_parse(yara_rules_string: str) -> List[List[Dict[str, Any]]]:
+    """Parse the given yara_rule string using plyara (see https://plyara.readthedocs.io/en/latest/)."""
+    parser = _plyara_parser()
+    try:
+        parser.parse_string(yara_rules_string)
+    except Exception:
+        message = 'The given text is not a valid yara rule.'
+        raise
+
+    return parser.rules
+
+
+# TODO: write and use a decorator that parses a yara rule
+def yara_rules_names(yara_rules_string: str) -> List[str]:
+    """Get the name(s) of the given yara_rule(s)."""
+    yara_rules_data = yara_rules_parse(yara_rules_string)
+    all_rule_names = []
+
+    for rule in yara_rules_data:
+        if rule.get('rule_name'):
+            rule_name = rule['rule_name']
+            all_rule_names.append(rule_name)
+
+    return all_rule_names
+
+
+def yara_rules_tags(yara_rules_string: str) -> List[str]:
+    """Get the tags for the given yara_rule(s)."""
+    yara_rules_data = yara_rules_parse(yara_rules_string)
+    all_rule_tags = []
+
+    for rule in yara_rules_data:
+        if rule.get('tags'):
+            rule_tags = rule['tags']
+            all_rule_tags.append(rule_tags)
+
+    return all_rule_tags
+
+
+def yara_rules_strings(yara_rules_string: str) -> List[List[Dict[str, Any]]]:
+    """Get the strings used in the given yara_rule(s)."""
+    yara_rules_data = yara_rules_parse(yara_rules_string)
+    all_rule_strings = []
+
+    for rule in yara_rules_data:
+        if rule.get('strings'):
+            yara_rule_strings = rule['strings']
+            all_rule_strings.append(yara_rule_strings)
+
+    return all_rule_strings
+
+
+def yara_rules_metadata(yara_rules_string: str) -> List[List[Dict[str, Any]]]:
+    """Get the metadata for the given yara_rule(s)."""
+    yara_rules_data = yara_rules_parse(yara_rules_string)
+    all_rule_metadata = []
+
+    for rule in yara_rules_data:
+        if rule.get('metadata'):
+            yara_rule_metadata = rule['metadata']
+            all_rule_metadata.append(yara_rule_metadata)
+
+    return all_rule_metadata
--- tests/test_democritus_yara.py
+++ tests/test_democritus_yara.py
@@ -1,10 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 """Tests for `democritus_yara` module."""
 
+import plyara
 import pytest
 
-from democritus_yara import is_yara_rule
+from democritus_yara import (
+    is_yara_rule,
+    yara_rules_find,
+    yara_rules_standardize,
+    yara_rules_parse,
+    yara_rules_names,
+    yara_rules_tags,
+    yara_rules_strings,
+    yara_rules_metadata,
+)
 
 
 SIMPLE_RULE_1 = 'rule MyRule { strings: $a="1" \n condition: false }'
@@ -28,3 +36,69 @@ def test_is_yara_rule_docs_1():
     assert is_yara_rule(SIMPLE_RULE_1)
     assert is_yara_rule(COMPLEX_RULE_1)
     assert not is_yara_rule('foo bar')
+
+
+def test_yara_rules_find_1():
+    results = yara_rules_find(SIMPLE_RULE_1)
+    assert results == ['rule MyRule { strings: $a="1" condition: false }']
+
+    results = yara_rules_find(COMPLEX_RULE_1)
+    assert len(results) == 1
+
+
+def test_yara_rules_standardize_1():
+    standardized_rule = yara_rules_standardize(SIMPLE_RULE_1)
+    assert isinstance(standardized_rule, list)
+    assert standardized_rule[0] == 'rule MyRule {\n\n\tstrings:\n\t\t$a = "1"\n\n\tcondition:\n\t\tfalse\n}\n'
+
+
+def test_multiple_rules_yara_rules_parse_1():
+    s = SIMPLE_RULE_1 + '\n\n' + COMPLEX_RULE_1
+    results = yara_rules_parse(s)
+    assert len(results) == 2
+
+
+def test_invalid_yara_rules_parse():
+    # try to parse an incomplete yara rule
+    with pytest.raises(plyara.exceptions.ParseTypeError):
+        yara_rules_parse(SIMPLE_RULE_1[5:])
+
+
+def test_yara_rules_parse_1():
+    results = yara_rules_parse(SIMPLE_RULE_1)
+    assert isinstance(results, list)
+    assert isinstance(results[0], dict)
+    assert results[0]['rule_name'] == 'MyRule'
+
+    results = yara_rules_parse(COMPLEX_RULE_1)
+    assert isinstance(results, list)
+    assert isinstance(results[0], dict)
+    assert results[0]['rule_name'] == 'silent_banker'
+
+
+def test_yara_rules_names_1():
+    assert yara_rules_names(SIMPLE_RULE_1) == ['MyRule']
+    assert yara_rules_names(COMPLEX_RULE_1) == ['silent_banker']
+
+
+def test_yara_rules_tags_1():
+    assert yara_rules_tags(SIMPLE_RULE_1) == []
+    assert yara_rules_tags(COMPLEX_RULE_1) == [['banker']]
+
+
+def test_yara_rules_strings_1():
+    assert yara_rules_strings(SIMPLE_RULE_1) == [[{'name': '$a', 'value': '1', 'type': 'text'}]]
+    assert yara_rules_strings(COMPLEX_RULE_1) == [
+        [
+            {'name': '$a', 'value': '{6A 40 68 00 30 00 00 6A 14 8D 91}', 'type': 'byte'},
+            {'name': '$b', 'value': '{8D 4D B0 2B C1 83 C0 27 99 6A 4E 59 F7 F9}', 'type': 'byte'},
+            {'name': '$c', 'value': 'UVODFRYSIHLNWPEJXQZAKCBGMT', 'type': 'text'},
+        ]
+    ]
+
+
+def test_yara_rules_metadata_1():
+    assert yara_rules_metadata(SIMPLE_RULE_1) == []
+    assert yara_rules_metadata(COMPLEX_RULE_1) == [
+        [{'description': 'This is just an example'}, {'thread_level': 3}, {'in_the_wild': True}]
+    ]
